4abc10f918cca826c225bfd4e1b06ab0f060fe17
junyuliu <junyuliu@usc.edu>
2020-06-25 10:24:43 -0700

    edit Junyu Hello World

--- Junyu_Liu_HelloWorld.java
+++ Junyu_Liu_HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
     }
 
     public void Hello(){
-    	System.out.println("Hello World!")
+    	System.out.println("Hello World!");
+    	System.out.println("Hello World!");
     }
 }

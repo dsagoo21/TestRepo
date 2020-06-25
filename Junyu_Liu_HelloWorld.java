f08de7225530eced0ad28338af05abadc0dd9beb
junyuliu <junyuliu@usc.edu>
2020-06-25 09:45:43 -0700

    Junyu added Hello World

---
+++ Junyu_Liu_HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        HelloWorld hello = new HelloWorld();
+        hello.Hello();
+    }
+
+    public void Hello(){
+    	System.out.println("Hello World!")
+    }
+}

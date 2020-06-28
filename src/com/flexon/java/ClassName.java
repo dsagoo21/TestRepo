3a6387155e5b529554323d92a4f9392b4a5ef0b8
TonyTheScrub <dli40@ucsc.edu>
2020-06-27 23:45:41 -0700

    array and rand # good

--- src/com/flexon/java/ClassName.java
+++ src/com/flexon/java/ClassName.java
@@ -3,5 +3,11 @@ package com.flexon.java;
 public class ClassName {
     public static void main(String[] args){
         System.out.println("hello world");
+        String[] thisClass = new String[10];
+        thisClass = new String[]{"Deep", "Dongshuo", "Shrina", "Abby", "Kun", "Oaz", "Jason", "Sam", "Richard", "Tao"};
+        //Generate random value
+        int rand = (int) (Math.random()*9);
+        System.out.println(thisClass[rand]);
+
     }
 }
